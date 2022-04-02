4e793c2b24de1181fcb761906dd84c781e4b774d
Beto2203 <lactoz22@gmail.com>
2022-04-01 20:31:38 -0600

    Just need to fix not connected bottom getting full

--- Percolation.java
+++ Percolation.java
@@ -6,68 +6,85 @@
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.Arrays;
+
 public class Percolation {
     private WeightedQuickUnionUF uf;
     private int[] grid;
     private int numOpen;
     private int nSize;
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        if (n < 1) { throw new IllegalArgumentException("Input must be bigger than zero"); }
+        if (n < 1) {
+            throw new IllegalArgumentException("Input must be bigger than zero");
+        }
         uf = new WeightedQuickUnionUF(n * n + 2);
-        grid = new int[n*n+1];
+        grid = new int[n * n + 1];
         nSize = n;
         for (int i = 1; i <= n; i++) {
             uf.union(0, i);
-            uf.union(n*n + 1, n*n - n + i);
+            uf.union(n * n + 1, n * n - n + i);
         }
     }
+
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validate(row, col);
+        System.out.println(Arrays.toString(grid));
 
-        int rowPosition = (row-1)*nSize;
+        int rowPosition = (row - 1) * nSize;
+
         if (!isOpen(row, col)) {
             grid[rowPosition + col] = 1;
             numOpen++;
 
-            if (row-1 > 0 && isOpen(row-1, col)) { uf.union(rowPosition-nSize + col, rowPosition + col); }
-            if (row+1 <= nSize && isOpen(row+1, col)) { uf.union(rowPosition+nSize + col, rowPosition + col); }
-            if (col-1 > 0 && isOpen(row, col-1)) { uf.union(rowPosition + col, rowPosition + col-1); }
-            if (col+1 <= nSize && isOpen(row, col+1)) { uf.union(rowPosition + col, rowPosition + col+1); }
 
+            if (row - 1 > 0 && isOpen(row - 1, col)) {
+                uf.union(rowPosition - nSize + col, rowPosition + col);
+            }
+            if (row + 1 <= nSize && isOpen(row + 1, col)) {
+                uf.union(rowPosition + nSize + col, rowPosition + col);
+            }
+            if (col - 1 > 0 && isOpen(row, col - 1)) {
+                uf.union(rowPosition + col, rowPosition + col - 1);
+            }
+            if (col + 1 <= nSize && isOpen(row, col + 1)) {
+                uf.union(rowPosition + col, rowPosition + col + 1);
+            }
         }
     }
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row, col);
 
-        return grid[(row-1)*nSize + col] == 1;
+        return grid[(row - 1) * nSize + col] == 1;
     }
+
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         validate(row, col);
 
-        return (isOpen(row, col) && uf.find(0) == uf.find((row - 1)*nSize + col));
+        return (isOpen(row, col) &&
+                uf.find(0) == uf.find((row - 1) * nSize + col));
     }
+
     // returns the number of open sites
     public int numberOfOpenSites() {
         return numOpen;
     }
+
     // does the system percolate?
     public boolean percolates() {
-        return uf.find(0) == uf.find(nSize*nSize+1);
+        return uf.find(0) == uf.find(nSize * nSize + 1);
     }
     private void validate(int row, int col) {
         if (row < 1 || col < 1 || row > nSize || col > nSize) {
             throw new IllegalArgumentException("Entered coordinates out of bounds");
         }
     }
+
     // test client (optional)
     public static void main(String[] args) {
-
-        Percolation dummy = new Percolation(10);
-
-        dummy.open(1, 1);
     }
 }
