91b68244e8393658292cdfd3956bd637f17bfe8c
Beto2203 <lactoz22@gmail.com>
2022-04-01 18:54:19 -0600

    Fixed open bug

--- Percolation.java
+++ Percolation.java
@@ -8,54 +8,47 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private WeightedQuickUnionUF uf;
-    private int[][] grid;
+    private int[] grid;
     private int numOpen;
     private int nSize;
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n < 1) { throw new IllegalArgumentException("Input must be bigger than zero"); }
         uf = new WeightedQuickUnionUF(n * n + 2);
-        grid = new int[n][n];
+        grid = new int[n*n+1];
         nSize = n;
         for (int i = 1; i <= n; i++) {
-            uf.union(0, 3);
-            // uf.union(n*n + 1, (n*n - n) + i);
+            uf.union(0, i);
+            uf.union(n*n + 1, n*n - n + i);
         }
     }
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validate(row, col);
 
+        int rowPosition = (row-1)*nSize;
         if (!isOpen(row, col)) {
-            grid[row-1][col-1] = 1;
+            grid[rowPosition + col] = 1;
             numOpen++;
 
-            if (row - 2 >= 0 && isOpen(row-1, col)) {
-                uf.union((row * col), (row-1)*(col));
-            }
-            if (row < nSize && isOpen(row+1, col)) {
-                uf.union((row * col), (row+1)*(col));
-            }
-            if (col - 2 >= 0 && isOpen(row, col-1)) {
-                uf.union((row * col), (row)*(col-1));
-            }
-            if (col < nSize && isOpen(row, col+1)) {
-                uf.union((row * col), (row)*(col+1));
-            }
-        }
+            if (row-1 > 0 && isOpen(row-1, col)) { uf.union(rowPosition-nSize + col, rowPosition + col); }
+            if (row+1 <= nSize && isOpen(row+1, col)) { uf.union(rowPosition+nSize + col, rowPosition + col); }
+            if (col-1 > 0 && isOpen(row, col-1)) { uf.union(rowPosition + col, rowPosition + col-1); }
+            if (col+1 <= nSize && isOpen(row, col+1)) { uf.union(rowPosition + col, rowPosition + col+1); }
 
+        }
     }
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row, col);
 
-        return grid[row-1][col-1] == 1;
+        return grid[(row-1)*nSize + col] == 1;
     }
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         validate(row, col);
 
-        return uf.find(row*col) == uf.find(0);
+        return (isOpen(row, col) && uf.find(0) == uf.find((row - 1)*nSize + col));
     }
     // returns the number of open sites
     public int numberOfOpenSites() {
@@ -70,7 +63,6 @@ public class Percolation {
             throw new IllegalArgumentException("Entered coordinates out of bounds");
         }
     }
-
     // test client (optional)
     public static void main(String[] args) {
 
